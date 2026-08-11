bd6e59f23d03970a86122db78a7b8a6a787e53f1
lisi <greenjim301@gmail.com>
2026-08-11 09:54:14 +0800

    unbuff log

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include "hand_lio/HandLioNode.h"
 
 int main(int argc, char** argv) {
+    std::setvbuf(stdout, nullptr, _IOLBF, 0);
+    
     ros::init(argc, argv, "hand_lio_node");
     ros::NodeHandle nh;
     ros::NodeHandle pnh("~");
